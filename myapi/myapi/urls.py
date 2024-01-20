8f47e6a5bde24d7648e517f8edd1758d8e789385
Harshal Jagtap <51406641+harshaljagtap6@users.noreply.github.com>
2024-01-21 00:48:04 +0530

    Delete myapi directory

--- myapi/myapi/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, re_path
-
-urlpatterns = [
-    re_path('admin/', admin.site.urls),
-    re_path(r'^', include('myapiapp.urls'))
-]
