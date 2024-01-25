64bd47f9ade32b9ca5337d092020d28e8f55dde7
Harshal Jagtap <51406641+harshaljagtap6@users.noreply.github.com>
2024-01-25 11:10:44 +0530

    Add files via upload

--- myapi/myapi/urls.py
+++ myapi/myapi/urls.py
@@ -20,6 +20,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     re_path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),name='schema_swagger_ui'),  #<-- Here
     re_path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),name='schema-redoc'),  #<-- Here
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path('admin/', admin.site.urls),
     re_path('', include('myapiapp.urls')),
 ]
